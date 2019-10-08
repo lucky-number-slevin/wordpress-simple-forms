21e3cdf68a5f5c711d08c182199544eb4d0d4a0e
Bakisa <bakisa@gmail.com>
2019-10-09 01:41:35 +0200

    Admin menu

--- src/Api/SettingsApi.php
+++ src/Api/SettingsApi.php
@@ -13,6 +13,10 @@ class SettingsApi {
    * @var array
    */
   private $adminPages = [];
+  /**
+   * @var array
+   */
+  private $adminSubpages = [];
 
   public function register() {
     if (!empty($this->adminPages)) {
@@ -22,12 +26,43 @@ class SettingsApi {
 
   public function addPages(array $pages) {
     $this->adminPages = $pages;
+    return $this; // for method chaining
+  }
+
+  public function addSubpages(array $subpages) {
+    $this->adminSubpages = array_merge($this->adminSubpages, $subpages);
+    return $this; // for method chaining
+  }
+
+  public function withSubPage(string $title = NULL) {
+    if (empty($this->adminPages)) {
+      return $this; // for method chaining
+    }
+
+    $admin_page = reset($this->adminPages);
+    $subpages = [
+      'parent_slug' => $admin_page['menu_slug'],
+      'page_title' => $admin_page['page_title'],
+      'menu_title' => $title ?? $admin_page['menu_title'],
+      'capability' => $admin_page['capability'],
+      'menu_slug' => $admin_page['menu_slug'],
+      'callback' => function () {
+        echo '<h1>Simple Forms Plugin Callback Function</h1>';
+      }
+    ];
+
+    $this->adminSubpages = [$subpages];
+
     return $this;
   }
 
   public function addAdminMenu() {
     foreach ($this->adminPages as $page) {
       add_menu_page($page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback'], $page['icon_url'], $page['position']);
+    }
+
+    foreach ($this->adminSubpages as $page) {
+      add_submenu_page($page['parent_slug'], $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback']);
     }
   }
 
--- src/Pages/Admin.php
+++ src/Pages/Admin.php
@@ -17,25 +17,53 @@ class Admin extends BaseController {
    * @var array
    */
   private $pages;
+  /**
+   * @var array
+   */
+  private $subpages;
 
   public function __construct() {
     parent::__construct();
     $this->settings = new SettingsApi();
+
     $this->pages = [
       'page_title' => 'Simple Forms',
       'menu_title' => 'Simple Forms',
       'capability' => 'manage_options',
       'menu_slug' => 'simple_forms',
       'callback' => function () {
-        echo '<h1>Simple Forms Plugin Callback Function</h1>';
+        echo '<h1>Simple Forms</h1>';
       },
       'icon_url' => 'dashicons-analytics',
       'position' => NULL
     ];
+
+    $this->subpages = [
+      [
+        'parent_slug' => $this->pages['menu_slug'],
+        'page_title' => 'Simple Forms - Add Form',
+        'menu_title' => 'Add Form',
+        'capability' => 'manage_options',
+        'menu_slug' => 'sp_add_form',
+        'callback' => function () {
+          echo '<h1>Simple Forms - Add Form</h1>';
+        }
+      ],
+      [
+        'parent_slug' => $this->pages['menu_slug'],
+        'page_title' => 'Simple Forms - Submissions',
+        'menu_title' => 'Dashboard',
+        'capability' => 'manage_options',
+        'menu_slug' => 'sp_dashboard',
+        'callback' => function () {
+          echo '<h1>Simple Forms - Dashboard</h1>';
+        }
+      ],
+    ];
   }
 
   function register() {
-    $this->settings->addPages([$this->pages])->register();
+    $this->settings->addPages([$this->pages])->withSubPage('Dashboard')->addSubpages($this->subpages)->register();
   }
 
 }
